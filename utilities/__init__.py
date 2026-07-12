a9e84e166c5e0c4fa7e6decbec97b01ef61b84ce
KiseraTimon <timonmarick@gmail.com>
2026-07-13 00:42:08 +0300

    bug: fix dependency ref error

--- utilities/__init__.py
+++ utilities/__init__.py
@@ -4,6 +4,5 @@ from .time_utils import timestp, get_timestamp_id
 from .error_extractor import extract_error_details
 from .sys_logger import log_system_info
 from .error_handler import log_critical_error
-from .mail_manager import send_email
 
 from .compatibility import *
